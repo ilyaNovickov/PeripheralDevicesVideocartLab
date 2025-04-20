00411a4d775a52b178c3ac83bd25195d6abf54ea
Ilya Novikov <ilyanovickov@mail.ru>
2025-04-20 23:40:01 +0300

    .

---
+++ src/VideocartLab/Videocart.Views/EventsArgs/NodeClickedArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Videocart.Views.EventsArgs
+{
+    public class NodeClickedArgs : EventArgs
+    {
+        public INodeView SenderNode { get; private set; }
+
+        public NodeClickedArgs(INodeView senderNode)
+        {
+            SenderNode = senderNode;
+        }
+    }
+}
