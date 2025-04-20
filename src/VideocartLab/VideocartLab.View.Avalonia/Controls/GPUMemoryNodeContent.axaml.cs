68a9efa1168a98366bb0679fd0fecb2e1ef99885
Ilya Novikov <ilyanovickov@mail.ru>
2025-04-20 15:15:45 +0300

    Добавлен тестовый класс для узла

---
+++ src/VideocartLab/VideocartLab.View.Avalonia/Controls/GPUMemoryNodeContent.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace VideocartLab.View.Avalonia.Controls;
+
+public partial class GPUMemoryNodeContent : UserControl
+{
+    public GPUMemoryNodeContent()
+    {
+        InitializeComponent();
+    }
+}
