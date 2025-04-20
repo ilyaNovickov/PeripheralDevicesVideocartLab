1502fb3cd615587df25246ff950014363440e60e
Ilya Novikov <ilyanovickov@mail.ru>
2025-04-20 14:52:57 +0300

    Добавлен проект Avalonia

---
+++ src/VideocartLab/VideocartLab.View.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace VideocartLab.View.Avalonia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
