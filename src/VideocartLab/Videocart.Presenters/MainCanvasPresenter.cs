3936bc13de04059374b0c869d0418664d60a67d3
Ilya Novikov <ilyanovickov@mail.ru>
2025-04-20 20:04:59 +0300

    .

--- src/VideocartLab/Videocart.Presenters/MainCanvasPresenter.cs
+++ src/VideocartLab/Videocart.Presenters/MainCanvasPresenter.cs
@@ -1,9 +1,19 @@
 ﻿using Videocart.Models;
+using Videocart.Views;
 
 namespace Videocart.Presenters
 {
     public class MainCanvasPresenter
     {
         private List<Node> nodes = new List<Node>();
+
+        private IMainCanvasView mainCanvasView;
+
+        public MainCanvasPresenter(IMainCanvasView mainCanvasView)
+        {
+            this.mainCanvasView = mainCanvasView;
+        }
+
+
     }
 }
---
+++ src/VideocartLab/Videocart.Views/EventsArgs/MousePressedArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Videocart.Views.EventsArgs
+{
+    public enum MouseButton
+    {
+        Left, Right, Middle, Undef
+    }
+
+    public class MousePressedArgs : EventArgs
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public MouseButton MouseButton { get; private set; }    
+
+        public MousePressedArgs(double x, double y, MouseButton mouseButton)
+        {
+            X = x;
+            Y = y;
+            MouseButton = mouseButton;
+        }
+    }
+}
