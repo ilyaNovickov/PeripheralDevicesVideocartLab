bd31bb58b88806c968ac77ec7463033e8619122e
Ilya Novikov <ilyanovickov@mail.ru>
2025-04-30 16:12:46 +0300

    Добавлен контроллер

---
+++ src/VideocartSol/VideocartLab.MainModelsProj/GPUController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideocartLab.MainModelsProj.GPUMemory;
+using VideocartLab.MainModelsProj.ConnectionInterface;
+using VideocartLab.MainModelsProj.Screen;
+
+namespace VideocartLab.MainModelsProj
+{
+    public class GPUController
+    {
+        private GPU? gpu;
+        private VRAM? vram;
+        private VideocartLab.MainModelsProj.ConnectionInterface.ConnectionInterface? connectionInterface;
+        private ScreenInterface? screenInterface;
+
+        public GPU? GPU
+        {
+            get => gpu;
+            set => gpu = value;
+        }
+
+        public VRAM? VRAM
+        {
+            get => vram;
+            set => vram = value;
+        }
+
+        public ConnectionInterface.ConnectionInterface? ConnectionInterface
+        {
+            get => connectionInterface;
+            set => connectionInterface = value;
+        }
+
+        public ScreenInterface? ScreenInterface
+        {
+            get => screenInterface;
+            set => screenInterface = value;
+        }
+    }
+}
