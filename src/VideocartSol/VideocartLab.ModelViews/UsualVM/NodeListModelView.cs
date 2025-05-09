87a08e15cc572f96a159512ee97deecf54afd8e8
Ilya Novikov <ilyanovickov@mail.ru>
2025-05-09 19:01:46 +0300

    тестирование списка

--- src/VideocartSol/VideocartLab.ModelViews/UsualVM/NodeListModelView.cs
+++ src/VideocartSol/VideocartLab.ModelViews/UsualVM/NodeListModelView.cs
@@ -9,14 +9,73 @@ namespace VideocartLab.ModelViews
 {
     public class NodeListModelView : ModelViewBase
     {
+        private ObservableCollection<NodeListItemModelView> items = new();
+        private NodeListItemModelView? selectedItem = null;
+
         public NodeListModelView() 
-        { 
-            Strs = new ObservableCollection<string>()
+        {
+            items = new ObservableCollection<NodeListItemModelView>()
             {
-                "foo1", "foo2", "foo3"
+                new NodeListItemModelView()
+                {
+                    Name = "foo1"
+                },
+                new NodeListItemModelView()
+                {
+                    Name = "foo2"
+                }
             };
         }
 
-        public ObservableCollection<string> Strs { get; set; }
+        public ObservableCollection<NodeListItemModelView> AvaibleNodes { get => items; }
+
+        public event EventHandler<SelectedNodeItemChangedArgs>? SelectedItemChanged;
+
+        public NodeListItemModelView? SelectedItem
+        {
+            get => selectedItem;
+            set
+            {
+                selectedItem = value;
+                OnPropertyChanged();
+                SelectedItemChanged?.Invoke(this, new SelectedNodeItemChangedArgs(selectedItem));
+            }
+        }
+    }
+
+    public class NodeListItemModelView : ModelViewBase
+    {
+        private string name = "";
+        private Type? node = null;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        internal Type? NodeType
+        {
+            get => node;
+            set
+            {
+                node = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public class SelectedNodeItemChangedArgs : EventArgs
+    {
+        public NodeListItemModelView? NewItem { get; private set; }
+
+        public SelectedNodeItemChangedArgs(NodeListItemModelView? item)
+        {
+            NewItem = item;
+        }
     }
 }
