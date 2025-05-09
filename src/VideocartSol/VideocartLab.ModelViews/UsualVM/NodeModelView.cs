0ecc65ccfa2f4f77e97b4fcc28555d3e66cfd4ec
Ilya Novikov <ilyanovickov@mail.ru>
2025-05-09 19:05:35 +0300

    Добавлена vm для node

---
+++ src/VideocartSol/VideocartLab.ModelViews/UsualVM/NodeModelView.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideocartLab.ModelViews.UsualVM
+{
+    public class NodeModelView : ModelViewBase
+    {
+        private double x;
+        private double y;
+        private double width;
+        private double height;
+        private ModelViewBase? innerContent = null;
+        private string? name = null;
+
+        #region Properties
+        public double X
+        {
+            get => x;
+            set
+            {
+                x = value;
+                OnPropertyChanged();
+            }
+        }
+        public double Y
+        {
+            get => y;
+            set
+            {
+                y = value;
+                OnPropertyChanged();
+            }
+        }
+        public double Width
+        {
+            get => width;
+            set
+            {
+                width = value;
+                OnPropertyChanged();
+            }
+        }
+        public double Height
+        {
+            get => height;
+            set
+            {
+                height = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ModelViewBase? InnerContent
+        {
+            get => innerContent;
+            set
+            {
+                innerContent = value;
+                OnPropertyChanged();
+            }
+        }
+        public string? Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+    }
+}
