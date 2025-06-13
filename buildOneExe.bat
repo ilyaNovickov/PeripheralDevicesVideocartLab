3913f2f1900059288293c734e9937bae89a782ae
ilyaNovickov <112414579+ilyaNovickov@users.noreply.github.com>
2025-06-13 21:48:10 +0300

    Add files via upload

---
+++ buildOneExe.bat
@@ -0,0 +1,2 @@
+cd src\VideocartSol\VideocartLab.Views.AvaloniaProj
+dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained true
